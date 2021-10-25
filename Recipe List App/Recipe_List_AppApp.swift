52465d966ba6deaf738838c9adf2ad6c703bfd30
Bryan Pham <bryan7pham1@gmail.com>
2021-10-25 09:07:41 +0700

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Bryan Pham on 2021-10-25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
